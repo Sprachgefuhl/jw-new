e63e8313a2303fa41b8cf94c0b50448e3fcde0b5
Sprachgefuhl <benjsbroja@gmail.com>
2026-06-17 18:12:21 +1000

    commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 require('dotenv').config();
+const express = require('express');
 const { getState, updateState } = require('./services/state');
 const scrapeSite = require('./services/scraper');
+const notifyUsers = require('./services/notify');
+const app = express();
+
+app.get('/run-check', async (req, res) => {
+	const result = await runCheck();
+	res.send(result);
+});
+
+app.get('/', (req, res) => {
+  res.send('OK');
+});
 
 async function runCheck() {
 	const oldState = await getState();
@@ -8,7 +20,11 @@ async function runCheck() {
 	
 	if (currentState !== oldState.hash) { // if new contect detected
 		const updated = await updateState(currentState);
-		console.log(updated);
-		// notify emails
+		await notifyUsers(['benjsbroja@gmail.com']);
+		return 'New content detected - users notified';
+	} else {
+		return 'Nothing new';
 	}
 }
+
+app.listen(process.env.PORT || 3000);
--- src/services/notify.js
+++ src/services/notify.js
@@ -1,8 +1,31 @@
-async function notify(message) {
-  console.log('CHANGE DETECTED:');
-  console.log(message);
+const nodemailer = require('nodemailer');
 
-  // later: email / discord / telegram
+async function notifyUsers(users) {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const usersEmails = users.join(',');
+
+  const mailOptions = {
+    from: 'JW New',
+    to: usersEmails,
+    subject: 'New content on JW.org!',
+    text: 'https://www.jw.org/en/whats-new/',
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log(`Emails sent:' ${info.response}`);
+    return info;
+  } catch (error) {
+    console.error(`Error sending emails: ${error}`);
+    throw error;
+  }
 }
 
-module.exports = { notify };
+module.exports = notifyUsers;
